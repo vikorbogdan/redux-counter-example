5e07449d1767ecb54beee36d57db54e7df1fa053
Vikor Bogdán <vikorbogdan@gmail.com>
2023-05-03 23:23:05 +0200

    added login state example to app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { decrement, increment } from "./actions";
 function App() {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
   return (
     <>
       counter: {counter}
       <button onClick={() => dispatch(increment(5))}>inc</button>
       <button onClick={() => dispatch(decrement(5))}>dec</button>
+      {isLogged && <h3>See after login</h3>}
     </>
   );
 }
